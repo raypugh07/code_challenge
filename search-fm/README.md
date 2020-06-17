19a760e40ec349fbe162c923da339592eeb3fd20
raypugh07 <pacmanmuzik88@gmail.com>
2020-06-16 20:05:19 -0500

    deployed

--- search-fm/README.md
+++ search-fm/README.md
@@ -9,3 +9,5 @@ Once the list renders a user may click in the "Info" link which will redirect to
 Future functions to add would include the ability to sort artists alphabetically through paging and sorting as well as searching within the page.
 
 I would also add error messaging so that a user is aware when something has gone wrong. That can be done by using a conditional statement. 
+
+Deployed at https://5ee96c21b2c8e7b60a138a3e--laughing-mahavira-6e59b5.netlify.app/
--- search-fm/src/api/api.js
+++ search-fm/src/api/api.js
@@ -2,6 +2,6 @@
 
 
 
-export const API ='http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&api_key=26a8672124e09449ce7d98a21024bb9a&format=json'
+export const API ='https://ws.audioscrobbler.com/2.0/?method=artist.getinfo&api_key=26a8672124e09449ce7d98a21024bb9a&format=json'
 
-export const API2 = 'http://ws.audioscrobbler.com/2.0/?method=artist.search&api_key=26a8672124e09449ce7d98a21024bb9a&format=json'
+export const API2 = 'https://ws.audioscrobbler.com/2.0/?method=artist.search&api_key=26a8672124e09449ce7d98a21024bb9a&format=json'
